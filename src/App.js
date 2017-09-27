818dcad002953f0cedda8efbab46e57b5fb24bf1
seanyusa <seanyusa@gmail.com>
2017-09-27 10:05:15 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <TeamDisplay style={{'width': '35%'}} backgroundColor="#393939" numberSteps={9} currentStep={9} skippedSteps={[]}/>
+        <TeamDisplay 
+          style={{'width': '35%'}} 
+          backgroundColor="#393939" 
+          numberSteps={9} 
+          currentStep={9} 
+          skippedSteps={[]}
+        />
       </div>
     );
   }
