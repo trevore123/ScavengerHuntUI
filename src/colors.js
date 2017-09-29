2761f24b4e9a576c386e4d1266638f8ac154b90c
seanyusa <seanyusa@gmail.com>
2017-09-28 20:00:08 -0500

    Added colors actually

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export let green = '#60d394';
+export let red = '#fd5c62';
+export let yellow = '#ffff9b';
+export let grey = '#a7a7a7';
+export let blackBlue = '#393939';
+export let white = '#fcfcfc';
